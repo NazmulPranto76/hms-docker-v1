e459b291db77dac14e665a4bd9881118e2d40e51
Nazmul Islam <131145938+nazmul-islam76@users.noreply.github.com>
2023-06-11 20:53:33 +0600

    Update config.php

--- hms/admin/include/config.php
+++ hms/admin/include/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER','192.168.20.228');
+define('DB_SERVER','naz-db.c5tsce7xtv8b.eu-west-3.rds.amazonaws.com');
 define('DB_USER','root');
-define('DB_PASS' ,'hms1234');
+define('DB_PASS' ,'bjit1234');
 define('DB_NAME', 'hms');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
