eaac96875ad06469e72ff15abe005e050a4c140d
Nazmul Islam <131145938+nazmul-islam76@users.noreply.github.com>
2023-06-11 20:52:43 +0600

    Update db-load.sh

--- scripts/db-load.sh
+++ scripts/db-load.sh
@@ -1,1 +1,1 @@
-mysql -h samin-db.c5tsce7xtv8b.eu-west-3.rds.amazonaws.com -u root --password="hms1234" tetris < /tmp/hms-db-load-script.sql
+mysql -h naz-db.c5tsce7xtv8b.eu-west-3.rds.amazonaws.com -u root --password="bjit1234" hms < /tmp/hms-db-load-script.sql
