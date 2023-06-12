fa71e2644c273a7104e9fca729a2ee2ecc8ed929
Nazmul Islam <131145938+nazmul-islam76@users.noreply.github.com>
2023-06-12 09:39:19 +0600

    Update hms-db-load-script.sql

--- hms-db-load-script.sql
+++ hms-db-load-script.sql
@@ -6,10 +6,6 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `updationDate` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `admin` (`id`, `username`, `password`, `updationDate`) VALUES
-(1, 'admin', 'Test@12345', '30-10-2022 11:42:05 AM');
-
-
 CREATE TABLE IF NOT EXISTS `appointment` (
   `id` int(11) NOT NULL,
   `doctorSpecialization` varchar(255) DEFAULT NULL,
@@ -22,9 +18,6 @@ CREATE TABLE IF NOT EXISTS `appointment` (
   `doctorStatus` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `appointment` (`id`, `doctorSpecialization`, `doctorId`, `userId`, `consultancyFees`, `appointmentDate`, `appointmentTime`, `userStatus`, `doctorStatus`) VALUES
-(1, 'ENT', 1, 1, 500, '2022-11-10', '12:45 PM', 1, 0);
-
 CREATE TABLE IF NOT EXISTS `doctors` (
   `id` int(11) NOT NULL,
   `specilization` varchar(255) DEFAULT NULL,
@@ -36,8 +29,6 @@ CREATE TABLE IF NOT EXISTS `doctors` (
   `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `doctors` (`id`, `specilization`, `doctorName`, `address`, `docFees`, `contactno`, `docEmail`, `password`) VALUES
-(1, 'ENT', 'Anuj kumar', 'A 123 XYZ Apartment Raj Nagar Ext Ghaziabad', '500', 142536250, 'anujk123@test.com', 'f925916e2754e5e03f75dd58a5733251');
 
 CREATE TABLE IF NOT EXISTS `doctorslog` (
   `id` int(11) NOT NULL,
@@ -49,33 +40,10 @@ CREATE TABLE IF NOT EXISTS `doctorslog` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-INSERT INTO `doctorslog` (`id`, `uid`, `username`, `userip`, `logout`, `status`) VALUES
-(20, NULL, 'gfdgdf', 0x3a3a3100000000000000000000000000,  NULL, 0),
-(26, 1, 'anujk123@test.com', 0x3a3a3100000000000000000000000000, '06-11-2022 06:50:28 PM', 1);
-
-
 CREATE TABLE IF NOT EXISTS `doctorspecilization` (
   `id` int(11) NOT NULL,
   `specilization` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-INSERT INTO `doctorspecilization` (`id`, `specilization`) VALUES
-(1, 'Orthopedics'),
-(2, 'Internal Medicine'),
-(3, 'Obstetrics and Gynecology'),
-(4, 'Dermatology'),
-(5, 'Pediatrics'),
-(6, 'Radiology'),
-(7, 'General Surgery'),
-(8, 'Ophthalmology'),
-(9, 'Anesthesia'),
-(10, 'Pathology'),
-(11, 'ENT'),
-(12, 'Dental Care'),
-(13, 'Dermatologists'),
-(14, 'Endocrinologists'),
-(15, 'Neurologists');
 
 CREATE TABLE IF NOT EXISTS `tblcontactus` (
   `id` int(11) NOT NULL,
@@ -87,10 +55,6 @@ CREATE TABLE IF NOT EXISTS `tblcontactus` (
   `IsRead` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `tblcontactus` (`id`, `fullname`, `email`, `contactno`, `message`, `AdminRemark`, `IsRead`) VALUES
-(1, 'Anuj kumar', 'anujk30@test.com', 1425362514, 'This is for testing purposes.   This is for testing purposes.This is for testing purposes.This is for testing purposes.This is for testing purposes.This is for testing purposes.This is for testing purposes.This is for testing purposes.This is for testing purposes.', NULL, NULL);
-
-
 CREATE TABLE IF NOT EXISTS `tblmedicalhistory` (
   `ID` int(10) NOT NULL,
   `PatientID` int(10) DEFAULT NULL,
@@ -101,10 +65,6 @@ CREATE TABLE IF NOT EXISTS `tblmedicalhistory` (
   `MedicalPres` mediumtext DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `tblmedicalhistory` (`ID`, `PatientID`, `BloodPressure`, `BloodSugar`, `Weight`, `Temperature`, `MedicalPres`) VALUES
-(1, 1, '80/120', '120', '85', '98', 'Test');
-
-
 CREATE TABLE IF NOT EXISTS `tblpage` (
   `ID` int(10) NOT NULL,
   `PageType` varchar(200) DEFAULT NULL,
@@ -114,11 +74,6 @@ CREATE TABLE IF NOT EXISTS `tblpage` (
   `MobileNumber` bigint(10) DEFAULT NULL,
   `OpenningTime` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-INSERT INTO `tblpage` (`ID`, `PageType`, `PageTitle`, `PageDescription`, `Email`, `MobileNumber`, `OpenningTime`) VALUES
-(1, 'aboutus', 'About Us', '<ul style=\"padding: 0px; margin-right: 0px; margin-bottom: 1.313em; margin-left: 1.655em;\" times=\"\" new=\"\" roman\";=\"\" font-size:=\"\" 14px;=\"\" text-align:=\"\" center;=\"\" background-color:=\"\" rgb(255,=\"\" 246,=\"\" 246);\"=\"\"><li style=\"text-align: left;\"><font color=\"#000000\">The Hospital Management System (HMS) is designed for Any Hospital to replace their existing manual, paper based system. The new system is to control the following information; patient information, room availability, staff and operating room schedules, and patient invoices. These services are to be provided in an efficient, cost effective manner, with the goal of reducing the time and resources currently required for such tasks.</font></li><li style=\"text-align: left;\"><font color=\"#000000\">A significant part of the operation of any hospital involves the acquisition, management and timely retrieval of great volumes of information. This information typically involves; patient personal information and medical history, staff information, room and ward scheduling, staff scheduling, operating theater scheduling and various facilities waiting lists. All of this information must be managed in an efficient and cost wise fashion so that an institutions resources may be effectively utilized HMS will automate the management of the hospital making it more efficient and error free. It aims at standardizing data, consolidating data ensuring data integrity and reducing inconsistencies.&nbsp;</font></li></ul>', NULL, NULL, NULL),
-(2, 'contactus', 'Contact Details', 'D-204, Hole Town South West, Delhi-110096,India', 'info@gmail.com', 1122334455, '9 am To 8 Pm');
 
 
 CREATE TABLE IF NOT EXISTS `tblpatient` (
@@ -133,10 +88,6 @@ CREATE TABLE IF NOT EXISTS `tblpatient` (
   `PatientMedhis` mediumtext DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `tblpatient` (`ID`, `Docid`, `PatientName`, `PatientContno`, `PatientEmail`, `PatientGender`, `PatientAdd`, `PatientAge`, `PatientMedhis`) VALUES
-(1, 1, 'Amit Kumar', 1231231230, 'amitk@gmail.com', 'male', 'New Delhi india', 35, 'NA');
-
-
 CREATE TABLE IF NOT EXISTS `userlog` (
   `id` int(11) NOT NULL,
   `uid` int(11) DEFAULT NULL,
@@ -145,11 +96,6 @@ CREATE TABLE IF NOT EXISTS `userlog` (
   `logout` varchar(255) DEFAULT NULL,
   `status` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `logout`, `status`) VALUES
-(1, 1, 'johndoe12@test.com', 0x3a3a3100000000000000000000000000, NULL, 1);
-
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
@@ -160,10 +106,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-INSERT INTO `users` (`id`, `fullName`, `address`, `city`, `gender`, `email`, `password`) VALUES
-(1, 'John Doe', 'A 123 ABC Apartment GZB 201017', 'Ghaziabad', 'male', 'johndoe12@test.com', 'f925916e2754e5e03f75dd58a5733251');
 
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
