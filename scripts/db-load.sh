dfaef0b488b3da1657e4f40ca14f08a6994580f0
Nazmul Islam <131145938+nazmul-islam76@users.noreply.github.com>
2023-06-12 07:54:51 +0600

    Delete scripts directory

--- scripts/db-load.sh
+++
@@ -1,1 +0,0 @@
-mysql -h naz-db.c5tsce7xtv8b.eu-west-3.rds.amazonaws.com -u root --password="bjit1234" hms < /tmp/hms-db-load-script.sql
--- scripts/install_dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if ! [ -x "$(command -v httpd)" ]; then 
-yum install -y httpd >&2
-fi
